8c6217c592ea87317be18a439e24da8b6acbc679
mikiyas16 <mikiyasmesfin16@gmail.com>
2024-05-30 01:36:38 -0700

    day3 done

--- www/main.js
+++ www/main.js
@@ -12,4 +12,39 @@ $(document).ready(function () {
 
     });
 
+    // Siri configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+    });
+
+    //Siri message animation
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+
+        },
+        out: {
+            effect: "",
+
+        },
+
+    });  
+    
+    // mic button click event
+
+    $("#MicBtn").click(function () {
+        eel.playAssistantSound()
+        $("#Oval").attr("hidden", true);
+        $("#SiriWave").attr("hidden", false);
+        eel.allcommands()()
+    });
+
 });    
