ed2ef9d1807dc8c3b352ec8a4f4eb574bcf40768
mikiyas16 <mikiyasmesfin16@gmail.com>
2024-06-28 09:54:29 -0700

    day7 done

--- engine/command.py
+++ engine/command.py
@@ -9,26 +9,11 @@ def speak(text):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices') 
     engine.setProperty('voice', voices[0].id)
-    engine.setProperty('rate', 174)
+    engine.setProperty('rate', 150)
     eel.DisplayMessage(text)
     engine.say(text)
     engine.runAndWait()
 
-for i in range(3):
-    
-    print("this particular device is password protected, please enter the require password")
-    a = input("Enter Password to open NOVA :- ")
-    pw_file = open("D:\\miki file\\atom codes\\jarvis miki\\nova\\password.txt","r")
-    pw = pw_file.read()
-    pw_file.close()
-    if (a==pw):
-        print("WELCOME SIR ! ")
-        break
-    elif (i==2 and a!=pw):
-        exit()
-        
-    elif (a!=pw):
-        print("incorrect password")
 
 def takecommand():
     r = sr.Recognizer()
@@ -55,20 +40,23 @@ def takecommand():
         return query.lower()
         
 @eel.expose
-def allCommands():    
+def allCommands():   
+     
+    try:
+        query = takecommand()
+        print(query)
     
-    query = takecommand()
-    print(query)
-    
-    if "open" in query:
-        from engine.features import openCommand
-        openCommand(query)
-    elif "on youtube":
-        from engine.features import PlayYoutube
-        PlayYoutube(query)
+        if "open" in query:
+            from engine.features import openCommand
+            openCommand(query)
+        elif "on youtube" or "በዩቲዩብ":
+            from engine.features import PlayYoutube
+            PlayYoutube(query)
             
-    else:
-        print("not run")    
+        else:
+            print("not run")    
+    except:
+        print("error")    
         
     eel.ShowHood()        
 
